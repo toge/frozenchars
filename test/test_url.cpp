3900a0b90c85670e9f3c453d3700a256149cafaa
toge <toge.mail@gmail.com>
2026-04-16 12:54:24 +0900

    RFC3986に準拠することを確認するテストケースを追加

--- test/test_url.cpp
+++ test/test_url.cpp
@@ -20,6 +20,20 @@ TEST_CASE("URL encode") {
     REQUIRE(encoded.sv() == "Hello%20World%21");
   }
 
+  SECTION("reserved characters") {
+    auto constexpr input = ":/?#[]@!$&'()*+,;="_fs;
+    auto constexpr encoded = url_encode(input);
+    static_assert(encoded.sv() == "%3A%2F%3F%23%5B%5D%40%21%24%26%27%28%29%2A%2B%2C%3B%3D");
+    REQUIRE(encoded.sv() == "%3A%2F%3F%23%5B%5D%40%21%24%26%27%28%29%2A%2B%2C%3B%3D");
+  }
+
+  SECTION("UTF-8 characters") {
+    auto constexpr input = "こんにちは"_fs;
+    auto constexpr encoded = url_encode(input);
+    static_assert(encoded.sv() == "%E3%81%93%E3%82%93%E3%81%AB%E3%81%A1%E3%81%AF");
+    REQUIRE(encoded.sv() == "%E3%81%93%E3%82%93%E3%81%AB%E3%81%A1%E3%81%AF");
+  }
+
   SECTION("NTTP exact size") {
     auto constexpr encoded = url_encode<"a b c"_fs>();
     static_assert(encoded.size() == 9);
@@ -43,6 +57,20 @@ TEST_CASE("URL decode") {
     REQUIRE(decoded.sv() == "a b c");
   }
 
+  SECTION("mixed case hex") {
+    auto constexpr input = "%3a%2F%3f%23"_fs;
+    auto constexpr decoded = url_decode(input);
+    static_assert(decoded.sv() == ":/?#");
+    REQUIRE(decoded.sv() == ":/?#");
+  }
+
+  SECTION("invalid percent sequences") {
+    auto constexpr input = "%G1%1G%1%%"_fs;
+    auto constexpr decoded = url_decode(input);
+    static_assert(decoded.sv() == "%G1%1G%1%%");
+    REQUIRE(decoded.sv() == "%G1%1G%1%%");
+  }
+
   SECTION("NTTP exact size") {
     auto constexpr decoded = url_decode<"a%20b%20c"_fs>();
     static_assert(decoded.size() == 5);
@@ -58,12 +86,6 @@ TEST_CASE("URL pipe adaptors") {
 }
 
 TEST_CASE("URL Query String") {
-  auto constexpr params = std::array{
-    std::pair{"key1", "value1"},
-    std::pair{"key2", "value2"},
-    std::pair{"key3", "value3"},
-  };
-
   auto constexpr query_string = make_querystring(
     "key1", "value1",
     "key2", 1,
@@ -84,12 +106,6 @@ TEST_CASE("URL Query String Tuples") {
 }
 
 TEST_CASE("URL Query String with URL encoded") {
-  auto constexpr params = std::array{
-    std::pair{"key1", "a b:c"},
-    std::pair{"key2", "value2"},
-    std::pair{"key3", "value3"},
-  };
-
   auto constexpr query_string = make_querystring(
     "key1", "a b:c",
     "key2", "value2",
