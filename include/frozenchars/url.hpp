873394c23652f3cbcd55d6a030264e33ce1fa412
toge <toge.mail@gmail.com>
2026-04-16 16:33:46 +0900

    keyもurl_encodeするように。

--- include/frozenchars/url.hpp
+++ include/frozenchars/url.hpp
@@ -13,7 +13,7 @@ namespace detail {
  * @brief Tがfreeze可能かを判定するコンセプト
  */
 template<typename T>
-concept Freezable = requires(T t) {
+concept Freezable = requires(T const& t) {
   { freeze(t).sv() } -> std::convertible_to<std::string_view>;
 };
 
@@ -23,40 +23,53 @@ concept Freezable = requires(T t) {
   - 例えば std::pair や std::tuple の要素がfreeze可能であればこのコンセプトを満たす
  */
 template<typename T>
-concept FreezablePair = requires(T t) {
+concept FreezablePair = requires(T const& t) {
   { freeze(std::get<0>(t)).sv() } -> std::convertible_to<std::string_view>;
   { freeze(std::get<1>(t)).sv() } -> std::convertible_to<std::string_view>;
+  requires std::tuple_size_v<std::remove_cvref_t<T>> == 2;
 };
 
+/**
+ * @brief キー・バリューペアからクエリ文字列を生成する
+ * @param args キーと値を交互に並べた引数列（偶数個必須）
+ * @return "key1=val1&key2=val2" 形式の文字列
+ */
 template <class Key, class Value>
 requires (Freezable<Key> && Freezable<Value>)
 auto consteval make_querystring_impl(Key&& key, Value&& value) {
-  return concat(freeze(key), "=", url_encode(freeze(value)));
+  return concat(
+    url_encode(freeze(key)), "=", url_encode(freeze(value))
+  );
 }
 
 template <class Key, class Value, class... Tail>
 requires (sizeof...(Tail) % 2 == 0 && Freezable<Key> && Freezable<Value>)
 auto consteval make_querystring_impl(Key&& key, Value&& value, Tail&&... tail) {
   return concat(
-    freeze(key), "=", url_encode(freeze(value)),
+    url_encode(freeze(key)), "=", url_encode(freeze(value)),
     "&",
-    make_querystring_impl(std::forward<Tail>(tail)...)
+    make_querystring_impl(tail...)
   );
 }
 
+/**
+ * @brief キー・バリューをまとめたタプルからクエリ文字列を生成する
+ * @param args キーと値を交互に並べたタプル
+ * @return "key1=val1&key2=val2" 形式の文字列
+ */
 template <FreezablePair T>
 auto consteval make_querystring_impl(T&& t) {
   return concat(
-    freeze(std::get<0>(t)), "=", url_encode(freeze(std::get<1>(t)))
+    url_encode(freeze(std::get<0>(t))), "=", url_encode(freeze(std::get<1>(t)))
   );
 }
 
-template <FreezablePair Head, FreezablePair... Tail>
+template <FreezablePair Head, class... Tail>
 auto consteval make_querystring_impl(Head&& head, Tail&&... tail) {
   return concat(
-    freeze(std::get<0>(head)), "=", url_encode(freeze(std::get<1>(head))),
+    url_encode(freeze(std::get<0>(head))), "=", url_encode(freeze(std::get<1>(head))),
     "&",
-    make_querystring_impl(std::forward<Tail>(tail)...)
+    make_querystring_impl(tail...)
   );
 }
 
@@ -70,7 +83,7 @@ auto consteval make_querystring_impl(Head&& head, Tail&&... tail) {
  */
 template<class ... Args>
 auto consteval make_querystring(Args&&... args) {
-  return concat("?", detail::make_querystring_impl(std::forward<Args>(args)...));
+  return concat("?", detail::make_querystring_impl(args...));
 }
 
 /**
@@ -82,7 +95,7 @@ auto consteval make_querystring(Args&&... args) {
 template<class ... Args>
 auto consteval make_querystring(std::tuple<Args...> const& t) {
   return concat("?", std::apply([](auto&&... args) {
-    return detail::make_querystring_impl(std::forward<decltype(args)>(args)...);
+    return detail::make_querystring_impl(args...);
   }, t));
 }
 
