ca8d68bcd478e10d81e7be9f5ad5ae50c857c356
toge <toge.mail@gmail.com>
2026-07-01 20:23:28 +0900

    fix: compile_fail コメントを日本語に修正

--- test/compile_fail/frozen_regex_max_strings_exceeded.cpp
+++ test/compile_fail/frozen_regex_max_strings_exceeded.cpp
@@ -3,5 +3,5 @@
 
 using namespace frozenchars::literals;
 
-// [a-z] is 26 chars -> MaxStrings=4 triggers overflow
+// [a-z] は26文字 → MaxStrings=4 で超過が発生する
 auto const r = frozenchars::frozen_regex<"[a-z]"_fs, 4>{};
