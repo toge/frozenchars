57c8984e8b9f7bf500f37813913a7e2f7613b71d
toge <toge.mail@gmail.com>
2026-09-02 00:53:17 +0900

    refactor: wasm32-wasip1 を vcpkg + wasi-sdk でビルド、catch2 は wasi 除外
    - triplets/wasm32-wasip1.cmake を追加 (VCPKG_CRAINLOAD で wasi-sdk-p1.cmake)
    - vcpkg.json: catch2 に platform "!wasm32" を付与し wasi では除外、glaze を
      依存に追加し wasip1 でも glaze が vcpkg 経由で取得できるようにする
    - .github/workflows/ci.yml: linux-wasi-minimal を vcpkg + wasi-sdk の
      wasm32-wasip1 ビルドに変更。シングルヘッダを使わない通常ビルド
      (cmake -DVCPKG_TARGET_TRIPLET=wasm32-wasip1 ...) で smoke の WebAssembly
      生成を検証

---
+++ triplets/wasm32-wasip1.cmake
@@ -0,0 +1,5 @@
+set(VCPKG_TARGET_ARCHITECTURE wasm32)
+set(VCPKG_CRT_LINKAGE static)
+set(VCPKG_LIBRARY_LINKAGE static)
+set(VCPKG_CMAKE_SYSTEM_NAME WASI)
+set(VCPKG_CHAINLOAD_TOOLCHAIN_FILE "/home/toge/vm/wasi-sdk/share/cmake/wasi-sdk-p1.cmake")
